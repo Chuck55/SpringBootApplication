f81f8be26b9a32fea93f26fcf48a23864e414128
Kyle Meng <kyle.j.meng@gmail.com>
2018-07-24 10:30:16 -0500

    Step 3; add request perameters to jsp

--- student-services/student-services/src/main/java/org/chuck55/studentservices/controller/LoginController.java
+++ student-services/student-services/src/main/java/org/chuck55/studentservices/controller/LoginController.java
@@ -1,21 +1,17 @@
 package org.chuck55.studentservices.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-
-    @RequestMapping(value="/login")
-    public String sayHello(){
+    @RequestMapping("/login")
+    public String loginMessage(@RequestParam String name, ModelMap model){
+        model.put("name", name);
         return "login";
     }
-
     @RequestMapping(value="/logout")
     public String sayGoodbye(){
         return "logout";
