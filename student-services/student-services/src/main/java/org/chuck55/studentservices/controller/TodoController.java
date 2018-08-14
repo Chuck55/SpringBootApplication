b9de3fd9dff05085dadae4e73da9690b8163636f
Kyle Meng <kyle.j.meng@gmail.com>
2018-08-14 10:00:43 -0500

    Step 7 Adding formatting

--- student-services/student-services/src/main/java/org/chuck55/studentservices/controller/TodoController.java
+++ student-services/student-services/src/main/java/org/chuck55/studentservices/controller/TodoController.java
@@ -1,17 +1,17 @@
 package org.chuck55.studentservices.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.chuck55.studentservices.model.Todo;
 import org.chuck55.studentservices.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -28,11 +28,18 @@ public class TodoController {
         model.put("todos", service.retrieveTodos(name));
         return "list-todos";
     }
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        // Date - dd/MM/yyyy
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(
+                dateFormat, false));
+    }
 
     @RequestMapping(value = "/add-todo", method = RequestMethod.GET)
     public String showAddTodoPage(ModelMap model) {
-        model.addAttribute("todo", new Todo(0, (String) model.get("name"), "Default Desc",
-                new Date(), false));
+        model.addAttribute("todo", new Todo(0, (String) model.get("name"),
+                "Default Desc", new Date(), false));
         return "todo";
     }
 
@@ -42,10 +49,31 @@ public class TodoController {
         return "redirect:/list-todos";
     }
 
+    @RequestMapping(value = "/update-todo", method = RequestMethod.GET)
+    public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+        Todo todo = service.retrieveTodo(id);
+        model.put("todo", todo);
+        return "todo";
+    }
+
+    @RequestMapping(value = "/update-todo", method = RequestMethod.POST)
+    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "todo";
+        }
+
+        todo.setUser((String) model.get("name"));
+
+        service.updateTodo(todo);
+
+        return "redirect:/list-todos";
+    }
+
     @RequestMapping(value = "/add-todo", method = RequestMethod.POST)
     public String addTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
 
-        if(result.hasErrors()){
+        if (result.hasErrors()) {
             return "todo";
         }
 
--- student-services/student-services/src/main/webapp/WEB-INF/jsp/list-todos.jsp
+++ student-services/student-services/src/main/webapp/WEB-INF/jsp/list-todos.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 
 <html>
 
@@ -14,28 +15,35 @@
 			<caption>Your todos are</caption>
 			<thead>
 				<tr>
-					<th align="center" width="25%">Description</th>
-                    <th align="center" width="40%">Target Date</th>
-                    <th align="center" width="35%">Is it Done?</th>
+					<th>Description</th>
+					<th>Target Date</th>
+					<th>Is it Done?</th>
+					<th></th>
 					<th></th>
 				</tr>
 			</thead>
 			<tbody>
 				<c:forEach items="${todos}" var="todo">
 					<tr>
-		                <td align="left">${todo.desc}</td>
-                        <td align="left">${todo.targetDate}</td>
-                        <td align="left">${todo.done}</td>
-						<td><a type="left" class="btn btn-warning" href="/delete-todo?id=${todo.id}">Delete</a></td>
+						<td>${todo.desc}</td>
+						<td><fmt:formatDate value="${todo.targetDate}" pattern="dd/MM/yyyy"/></td>
+						<td>${todo.done}</td>
+						<td><a type="button" class="btn btn-success"
+							href="/update-todo?id=${todo.id}">Update</a></td>
+						<td><a type="button" class="btn btn-warning"
+							href="/delete-todo?id=${todo.id}">Delete</a></td>
 					</tr>
 				</c:forEach>
 			</tbody>
 		</table>
-		<div> <a class="button" href="/add-todo">Add a Todo</a></div>
-
-		<script src="webjars/jquery/1.9.1/jquery.min.js"></script>
-		<script src="webjars/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<div>
+			<a class="button" href="/add-todo">Add a Todo</a>
+		</div>
 	</div>
+
+	<script src="webjars/jquery/1.9.1/jquery.min.js"></script>
+	<script src="webjars/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
